c94f3a9d5a3f6e9fbc1608d44473bc821d6720e5
Michael Haase <darkstar@gernox.de>
2020-04-27 11:16:21 +0200

    Disable puppetserver autosign certificates

--- manifests/puppet/master.pp
+++ manifests/puppet/master.pp
@@ -7,4 +7,13 @@ class profile::puppet::master (
   contain profile::puppet::puppetdb
   contain profile::puppet::r10k
 
+  ini_setting { 'puppetserver autosign':
+    ensure  => present,
+    path    => $settings::config,
+    section => 'master',
+    setting => 'autosign',
+    value   => false,
+    notify  => Service['puppetserver'],
+  }
+
 }
