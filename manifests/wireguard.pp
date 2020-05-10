44d97655356021ecf0b586440031c421fd83bc4a
Michael Haase <darkstar@gernox.de>
2020-05-10 09:50:46 +0200

    Add type wireguard_peer

--- manifests/wireguard.pp
+++ manifests/wireguard.pp
@@ -5,8 +5,25 @@
 #   Interface configurations
 #
 class profile::wireguard (
+  String $public_key,
+  String $private_key,
+  String $ip,
   Hash $interfaces,
+
+  String $fqdn       = $profile::fqdn,
+  String $primary_ip = $profile::primary_ip,
+  Hash $nodes        = $profile::nodes,
 ) {
+
+  @@profile::wireguard_peer { "peer_${fqdn}":
+    ensure      => present,
+    internal_ip => $ip,
+    external_ip => $primary_ip,
+    public_key  => $public_key,
+  }
+
+  Profile::Wireguard_peer <<| name != "peer_${fqdn}" |>>
+
   class { '::wireguard':
     interfaces => $interfaces,
     require    => Profile::Package['linux-headers-generic'],
