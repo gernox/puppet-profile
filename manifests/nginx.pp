4afeeb9c3a7a6d235fad24d1f1c9eadaf5335839
Michael Haase <darkstar@gernox.de>
2020-05-17 07:32:53 +0200

    Add servers param to nginx

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,10 +1,15 @@
 # @summary
 #   Manages nginx configuration
 #
+# @param servers
+#
 class profile::nginx (
+  Hash $servers = {},
 ) {
   class { '::nginx':
     confd_purge  => true,
     server_purge => true,
   }
+
+  create_resources('nginx::resource::server', $servers)
 }
