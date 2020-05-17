f067234fb3651ba3f3866b9a1d46f9f1f1636e2b
Michael Haase <darkstar@gernox.de>
2020-05-17 08:10:09 +0200

    Configure nginx on puppet_master

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -4,12 +4,30 @@
 # @param servers
 #
 class profile::nginx (
+  String $dhparam,
   Hash $servers = {},
 ) {
   class { '::nginx':
-    confd_purge     => true,
-    server_purge    => true,
-    http_access_log => 'off',
+    confd_purge               => true,
+    server_purge              => true,
+    http_access_log           => 'off',
+    ssl_protocols             => 'TLSv1.2 TLSv1.3',
+    ssl_ciphers               =>
+      'ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384'
+    ,
+    ssl_prefer_server_ciphers => 'off',
+    ssl_stapling              => 'on',
+    ssl_stapling_verify       => 'on',
+    ssl_dhparam               => '/etc/nginx/dhparam.pem',
+  }
+
+  file { '/etc/nginx/dhparam.pem':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => $dhparam,
+    require => Class['nginx'],
+    notify  => Service['nginx'],
   }
 
   create_resources('nginx::resource::server', $servers)
