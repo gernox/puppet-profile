bd6fe02cc7fc8b6556ea226b25112491cde07022
Michael Haase <darkstar@gernox.de>
2020-05-17 20:03:21 +0200

    Allow node_exporter access from prometheus server

--- manifests/prometheus/node_exporter.pp
+++ manifests/prometheus/node_exporter.pp
@@ -6,11 +6,19 @@
 # @param node_exporter_options
 #
 class profile::prometheus::node_exporter (
+  Stdlib::Compat::Ip_address $prometheus_ip,
   String $version,
   String $options,
 ) {
   class { '::prometheus::node_exporter':
     version       => $version,
     extra_options => $options,
+  }
+
+  firewall { "110 IPv4 allow Prometheus access from ${prometheus_ip}":
+    source => $prometheus_ip,
+    dport  => 9100,
+    proto  => 'tcp',
+    action => 'accept',
   }
 }
