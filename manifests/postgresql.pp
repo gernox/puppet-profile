24a43800cedbce693acad82aa1921605191e1574
Michael Haase <darkstar@gernox.de>
2020-05-21 20:01:54 +0200

    Add postgresql backup

--- manifests/postgresql.pp
+++ manifests/postgresql.pp
@@ -28,4 +28,6 @@ class profile::postgresql (
       * => $v,
     }
   }
+
+  contain profile::postgresql::backup
 }
---
+++ manifests/postgresql/backup.pp
@@ -0,0 +1,75 @@
+# @summary
+#   Manages the PostgreSQL backups
+#
+# @param password
+#
+# @param base_dir
+#
+class profile::postgresql::backup (
+  String $backup_time,
+  String $password = $profile::postgresql::backup::password,
+  String $base_dir = $profile::base_dir,
+) {
+  $backup_dir = "${base_dir}/backups/postgresql"
+
+  file { '/home/postgres/.pgpass':
+    owner   => 'postgres',
+    group   => 'postgres',
+    mode    => '0600',
+    content => "localhost:5432:*:postgres:${password}",
+    require => Class['postgresql::server'],
+  }
+
+  file { $backup_dir:
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'postgres',
+    mode    => '0770',
+    require => Class['postgresql::server'],
+  }
+  -> file { "${backup_dir}/pg_backup.sh":
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///modules/profile/postgresql/pg_backup.sh',
+  }
+  -> file { "${backup_dir}/pg_backup_rotated.sh":
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///modules/profile/postgresql/pg_backup_rotated.sh',
+  }
+  -> file { "${backup_dir}/pg_backup.sh":
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('profile/postgresql/pg_backup.config.erb'),
+  }
+
+  # Systemd service and timer
+  file { '/etc/systemd/system/pg_backup.service':
+    ensure  => present,
+    content => file('profile/postgresql/pg_backup.service.erb'),
+    notify  => Service['pg_backup'],
+  }
+
+  file { '/etc/systemd/system/pg_backup.timer':
+    ensure  => present,
+    content => file('profile/postgresql/pg_backup.timer.erb'),
+    notify  => Service['pg_backup'],
+  }
+
+  service { 'pg_backup':
+    ensure  => running,
+    enable  => true,
+    name    => 'pg_backup.timer',
+    require => [
+      File["${backup_dir}/pg_backup.sh"],
+      File['/etc/systemd/system/pg_backup.service'],
+      File['/etc/systemd/system/pg_backup.timer'],
+    ],
+  }
+}
