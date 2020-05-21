a3ead3e300ca321ec0c6b63d54dff4553cabc319
Michael Haase <darkstar@gernox.de>
2020-05-21 16:28:04 +0200

    Add postgresql profile

---
+++ manifests/postgresql.pp
@@ -0,0 +1,31 @@
+# @summary
+#   Manages a PostgreSQL server
+#
+# @param version
+#
+# @param password
+#
+# @param dbs
+#
+class profile::postgresql (
+  String $version,
+  String $password,
+  Hash $dbs = {},
+) {
+  class { 'postgresql::globals':
+    encoding            => 'UTF-8',
+    locale              => 'en_US.UTF-8',
+    manage_package_repo => true,
+    version             => $version,
+  }
+
+  class { 'postgresql::server':
+    postgres_password => $password,
+  }
+
+  $dbs.each |$k, $v| {
+    postgresql::server::db { $k:
+      * => $v,
+    }
+  }
+}
---
+++ spec/classes/postgresql_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe 'profile::postgresql' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      let(:params) do
+        {
+          version: '9.1',
+          password: 'passwd',
+          dbs: {},
+        }
+      end
+
+      it { is_expected.to compile.with_all_deps }
+    end
+  end
+end
