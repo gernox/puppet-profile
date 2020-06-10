0025a15d877a4dbd60f84edb3732863d57a7ed8c
Michael Haase <darkstar@gernox.de>
2020-06-10 20:32:08 +0200

    Add nextcloud

--- manifests/php.pp
+++ manifests/php.pp
@@ -3,6 +3,8 @@
 #
 class profile::php (
   String $version,
+  Hash $extensions,
+  Hash $settings,
 ) {
   class { '::php::globals':
     php_version => $version,
@@ -13,31 +15,7 @@ class profile::php (
     fpm          => true,
     dev          => true,
     composer     => true,
-    extensions   => {
-      apcu  => {
-        package_name => 'php-apcu',
-      },
-      bcmath   => {},
-      bz2      => {},
-      curl     => {},
-      gd       => {},
-      gmp      => {},
-      imagick  => {
-        package_name => 'php-imagick',
-      },
-      intl     => {},
-      mbstring => {},
-      pgsql    => {},
-      xmlrpc   => {},
-      zip      => {},
-    },
-    settings     => {
-      'PHP/max_execution_time'  => '90',
-      'PHP/max_input_time'      => '300',
-      'PHP/memory_limit'        => '64M',
-      'PHP/post_max_size'       => '32M',
-      'PHP/upload_max_filesize' => '32M',
-      'Date/date.timezone'      => 'Europe/Berlin',
-    },
+    extensions   => $extensions,
+    settings     => $settings,
   }
 }
