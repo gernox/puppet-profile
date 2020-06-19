0a8b7ae530081c42e9cae9170b197684476ae49d
Michael Haase <darkstar@gernox.de>
2020-06-19 14:29:21 +0200

    Refactor bareos director

--- manifests/backup/server.pp
+++ manifests/backup/server.pp
@@ -59,38 +59,6 @@ class profile::backup::server (
         'Runs on Failure' => 'No',
       }
     },
-    'DefaultJob'                   => {
-      type                     => 'Backup',
-      file_set                 => 'LinuxDefault',
-      pool                     => 'Incremental',
-      messages                 => 'Standard',
-      priority                 => '10',
-      accurate                 => true,
-      write_bootstrap          => '/var/lib/bareos/%c.bsr',
-      full_backup_pool         => 'Full',
-      differential_backup_pool => 'Differential',
-      incremental_backup_pool  => 'Incremental',
-    },
-    'BackupBareosCatalog'          => {
-      description     => 'Backup the catalog database (after the nightly save)',
-      job_defs        => 'DefaultJob',
-      level           => 'Full',
-      file_set        => 'BareosCatalog',
-      schedule_res    => 'WeeklyCycleAfterBackup',
-      run_before_job  => '/usr/lib/bareos/scripts/make_catalog_backup.pl MyCatalog',
-      run_after_job   => '/usr/lib/bareos/scripts/delete_catalog_backup',
-      write_bootstrap => '|/usr/bin/bsmtp -h localhost -f \"\(Bareos\) \" -s \"Bootstrap for Job %j\" root@localhost',
-      priority        => 11,
-    },
-    'RestoreFiles'                 => {
-      description => 'Standard Restore template. Only one such job is needed for all standard Jobs/Clients/Storage ...',
-      type        => 'restore',
-      file_set    => 'LinuxAll',
-      storage     => 'File',
-      pool        => 'Incremental',
-      messages    => 'Standard',
-      where       => '/tmp/bareos-restores',
-    },
   }
 
   $pools = {
