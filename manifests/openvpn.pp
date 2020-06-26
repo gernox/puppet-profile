db6e6d4f639580ba177cda27e0164abcd47822c2
Michael Haase <darkstar@gernox.de>
2020-06-26 20:06:58 +0200

    Add openvpn

---
+++ manifests/openvpn.pp
@@ -0,0 +1,95 @@
+# @summary
+#   Manages the OpenVPN server configuration
+#
+class profile::openvpn (
+  String $network,
+  Optional[String] $network_ipv6 = '',
+
+  String $cipher,
+  String $tls_cipher,
+  String $config_dir,
+
+  String $user,
+
+  String $country,
+  String $province,
+  String $city,
+  String $organization,
+  String $email,
+
+  Array $dhcp_options            = [],
+  Array $push_options            = [],
+  Optional[Array] $routes        = [],
+  Optional[Array] $routes_ipv6   = [],
+  Optional[Hash] $options        = {},
+  Optional[Hash] $clients        = {},
+  Optional[Hash] $client_conf    = {},
+  Optional[Hash] $revokes        = {},
+) {
+  user { $user:
+    system     => true,
+    shell      => '/usr/sbin/nologin',
+    managehome => false,
+  }
+
+  class { '::openvpn':
+    autostart_all                   => true,
+    manage_service                  => true,
+
+    client_defaults                 => {
+      server         => 'gernox_internal',
+      remote_host    => 'vpn.gernox.de',
+      cipher         => $cipher,
+      tls_cipher     => $tls_cipher,
+      tls_auth       => true,
+      custom_options => {
+        auth            => 'SHA512',
+        key-direction   => '1',
+        script-security => '2',
+        down-pre        => '',
+      },
+    },
+    clients                         => $clients,
+    client_specific_config_defaults => {
+      ensure       => present,
+      server       => 'openvpn_server',
+      dhcp_options => $dhcp_options,
+    },
+    revokes                         => $revokes,
+    revoke_defaults                 => {
+      server => 'gernox_internal',
+    },
+    client_specific_configs         => $client_conf,
+
+    server_defaults                 => {
+      local          => '',
+      proto          => 'tcp',
+      c2c            => true,
+      country        => $country,
+      province       => $province,
+      city           => $city,
+      organization   => $organization,
+      email          => $email,
+      user           => $user,
+      group          => $user,
+      persist_key    => true,
+      persist_tun    => true,
+      cipher         => $cipher,
+      tls_cipher     => $tls_cipher,
+      tls_auth       => true,
+      keepalive      => '10 60',
+      custom_options => $options,
+    },
+    servers                         => {
+      gernox_internal => {
+        topology       => 'subnet',
+        server         => $network,
+        server_ipv6    => $network_ipv6,
+        route          => $routes,
+        route_ipv6     => $routes_ipv6,
+        push           => $push_options,
+        crl_auto_renew => true,
+      },
+    },
+  }
+}
---
+++ spec/classes/openvpn_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe 'profile::openvpn', type: :class do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+      let(:params) do
+        {
+          cipher: 'AES-256-CBC',
+          tls_cipher: 'TLS-DHE-RSA-WITH-AES-256-GCM-SHA384',
+          config_dir: '/etc/openvpn',
+          user: 'openvpn_server',
+          network: '10.200.200.0 255.255.255.0',
+          network_ipv6: 'fe80:1337:1337:1337::/64',
+          routes: ['192.168.30.0 255.255.255.0', '192.168.35.0 255.255.0.0'],
+          routes_ipv6: ['2001:db8:1234::/64', '2001:db8:abcd::/64'],
+          country: 'CO',
+          province: 'ST',
+          city: 'Some City',
+          organization: 'example.org',
+          email: 'testemail@example.org'
+        }
+      end
+
+      it { is_expected.to compile.with_all_deps }
+    end
+  end
+end
