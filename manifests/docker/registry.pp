b10cf7b506d409c5874be5add2bbf8301543afac
Michael Haase <darkstar@gernox.de>
2020-07-10 21:16:57 +0200

    Mount docker registry storage from host

--- manifests/docker/registry.pp
+++ manifests/docker/registry.pp
@@ -9,11 +9,17 @@ class profile::docker::registry (
   String $version,
   Integer $port,
   Boolean $manage_nginx = false,
+  String $base_dir      = $profile::base_dir,
 ) {
   contain ::gernox_docker
 
+  $registry_base_dir = "${base_dir}/data/registry"
+
   $network_name = 'registry-network'
   $bridge_name = 'br-registry'
+
+  profile::tools::create_dir { $registry_base_dir:
+  }
 
   ::docker::image { 'registry':
     ensure    => present,
@@ -37,7 +43,11 @@ class profile::docker::registry (
     ports                 => [
       "127.0.0.1:${port}:5000",
     ],
+    volumes               => [
+      "${registry_base_dir}:/var/lib/registry",
+    ],
     net                   => $network_name,
+    require               => Profile::Tools::Create_dir[$registry_base_dir],
   }
 
   if $manage_nginx {
