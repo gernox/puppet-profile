2d914d16211d1d1579a8c3e2dfe209746afe73d4
Michael Haase <darkstar@gernox.de>
2020-07-11 08:01:02 +0200

    Add drone

---
+++ manifests/drone.pp
@@ -0,0 +1,80 @@
+# @summary
+#   Manages drone
+#
+#
+class profile::drone (
+  String $version,
+  String $db_host,
+  Integer $db_port,
+  String $db_user,
+  String $db_password,
+  String $db_name,
+  String $network_name,
+  String $bridge_name,
+  Integer $http_port,
+  String $gitea_server,
+  String $gitea_client_id,
+  String $gitea_client_secret,
+  String $rpc_secret,
+  String $base_dir      = $profile::base_dir,
+  Boolean $manage_nginx = false,
+) {
+  contain ::gernox_docker
+  contain ::profile::postgresql
+
+  $drone_base_dir = "${base_dir}/data/drone"
+
+  profile::tools::create_dir { $drone_base_dir:
+  }
+
+  profile::postgresql::db { $db_name:
+    user     => $db_user,
+    password => $db_password,
+  }
+
+  ::docker::image { 'drone':
+    ensure    => present,
+    image     => 'drone/drone',
+    image_tag => $version,
+  }
+
+  docker_network { $network_name:
+    ensure  => present,
+    options => "com.docker.network.bridge.name=${bridge_name}",
+  }
+
+  firewall { "002 - IPv4: accept all ${bridge_name}":
+    iniface => $bridge_name,
+    action  => 'accept',
+  }
+
+  ::docker::run { 'drone':
+    image                 => "drone/drone:${version}",
+    health_check_interval => 30,
+    ports                 => [
+      "127.0.0.1:${http_port}:80",
+    ],
+    env                   => [
+      "DRONE_GITEA_SERVER=${gitea_server}",
+      "DRONE_GITEA_CLIENT_ID=${gitea_client_id}",
+      "DRONE_GITEA_CLIENT_SECRET=${gitea_client_secret}",
+      "DRONE_RPC_SECRET=${rpc_secret}",
+      'DRONE_GIT_ALWAYS_AUTH=true',
+      'DRONE_SERVER_PROTO=https',
+      'DRONE_SERVER_HOST=drone.cubieserver.de',
+      'DRONE_DATABASE_DRIVER=postgres',
+      "DRONE_DATABASE_DATASOURCE=postgres://${db_user}:${db_password}@${db_host}:${db_port}/${db_name}?sslmode=disable",
+    ],
+    volumes               => [
+      "${drone_base_dir}:/data",
+      '/var/run/docker.sock:/var/run/docker.sock',
+    ],
+    net                   => $network_name,
+    require               => File[$drone_base_dir],
+  }
+
+  if $manage_nginx {
+    contain ::profile::drone::nginx
+  }
+
+}
---
+++ manifests/drone/nginx.pp
@@ -0,0 +1,32 @@
+# @summary
+#   Manages the nginx configuration for drone
+#
+# @param http_domain
+#
+# @param http_port
+#
+class profile::drone::nginx (
+  String $http_domain,
+
+  Integer $http_port = $profile::drone::http_port,
+) {
+  contain profile::nginx
+
+  nginx::resource::server { 'drone':
+    server_name      => [
+      $http_domain,
+    ],
+    listen_port      => 443,
+    format_log       => 'anonymized',
+    ssl              => true,
+    ssl_cert         => '/etc/ssl/certs/gernox_de.crt',
+    ssl_key          => '/etc/ssl/private/gernox_de.key',
+    proxy            => "http://localhost:${http_port}",
+    proxy_set_header => [
+      'Host $http_host',
+      'X-Real-IP $remote_addr',
+      'X-Forwarded-For $proxy_add_x_forwarded_for',
+      'X-Forwarded-Proto $scheme',
+    ],
+  }
+}
