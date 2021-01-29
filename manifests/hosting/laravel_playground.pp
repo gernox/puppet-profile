f25975b61188679921fecc236b7dd8d1dc4f2056
Michael Haase <darkstar@gernox.de>
2021-01-29 17:35:40 +0100

    Add env file

--- manifests/hosting/laravel_playground.pp
+++ manifests/hosting/laravel_playground.pp
@@ -4,8 +4,15 @@
 class profile::hosting::laravel_playground {
   contain ::gernox_docker
 
+  $base_dir = "/tmp"
+
+  file { "${base_dir}/.env":
+    content => "TAG=14.04",
+  }
+
   docker_compose { 'laravel_playground':
     ensure        => present,
-    compose_files => ['/tmp/docker-compose.yaml'],
+    compose_files => ["${base_dir}/docker-compose.yaml"],
+    require       => File["${base_dir}/.env"]
   }
 }
