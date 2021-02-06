5d5ca631c190368e99c0c3cf4e4bcfacfc1e9967
Michael Haase <darkstar@gernox.de>
2021-02-06 08:09:23 +0100

    Add option to enable management of php repos

--- manifests/php.pp
+++ manifests/php.pp
@@ -6,13 +6,14 @@ class profile::php (
   Hash $fpm_pools,
   Hash $extensions,
   Hash $settings,
+  Boolean $manage_repos,
 ) {
   class { '::php::globals':
     php_version => $version,
     config_root => "/etc/php/${version}",
   }
   -> class { '::php':
-    manage_repos => false,
+    manage_repos => $manage_repos,
     fpm          => true,
     fpm_pools    => $fpm_pools,
     dev          => true,
